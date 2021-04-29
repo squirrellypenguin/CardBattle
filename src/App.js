7df22c4ae15a3217b806d3264bb26c58d76cbef3
Ted Teah <from@dabronx.nyc>
2021-04-29 11:21:11 -0400

    fixes

--- src/App.js
+++ src/App.js
@@ -1,56 +1,60 @@
 import './App.css';
-import { Route , Switch } from 'react-router-dom';
-import React, {useState, useEffect} from 'react';
+import {
+  Route,
+  Switch
+} from 'react-router-dom';
+import React, {
+  useState,
+  useEffect
+} from 'react';
 
 
 
 function App() {
   const [people, setPeople] = useState([])
-  let data =""
+  let data = ""
   const url = 'https://swapi.dev/api/people/'
-  
-const offset = 82
 
   // Retrieves the count for the entries 
   const getCount = async () => {
     const response = await fetch(url)
     data = await response.json()
-    console.log(data)
+    console.log(data.count)
     console.log(...data.results)
- 
+    return (
+      <h1>{data.count}</h1>
+    )
   }
 
   const getPeople = async () => {
-    let storage =[]
-    for (let i = 0; i < 5; i++){
-      let offset = i +1
-     const response = await fetch(`${url}${offset}`)
-    let data = await response.json()
-    storage.push(data)
-    console.log(storage)
-    // setPeople(...people, data)
-    // const storage = [{}]
-    // let foo = storage.push(data)
-    // console.log(storage)
-    // setPeople( [ ...data] )
-  //  console.log(storage)
+    const storage = []
+    for (let i = 0; i < 5; i++) {
+      let offset = i + 1
+      const response = await fetch(`${url}${offset}`)
+      let data = await response.json()
+      storage.push(data)
+      console.log(storage)
+      // setPeople(...people, data)
+      // const storage = [{}]
+      // let foo = storage.push(data)
+      // console.log(storage)
+      // setPeople( [ ...data] )
+      //  console.log(storage)
+    }
+
+
+    setPeople(storage)
+    // setPeople( [...people, data] )
   }
-  
-   setPeople(storage)
-  // setPeople( [...people, data] )
-  }
+
 
   useEffect(() => {
     getCount();
-    getPeople()
+    // getPeople();
   }, []);
- 
-  return (
-    <div className="App">
-      <header className="App-header">
-    <p>Header</p>
-      </header>
 
+  return ( <div className = "App" >
+  <h1> Star Wars</h1>
     </div>
   );
 }
