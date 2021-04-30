eac76ad7f07f54b1bd632727396b11145b011eeb
Ted Teah <from@dabronx.nyc>
2021-04-29 20:56:31 -0400

    new indivudal pages

---
+++ src/pages/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Main = (props) => {
+
+        //
+
+    return (
+
+        <div>
+        <h1>MAIN APP</h1>
+        </div>
+
+    )
+}
+
+export default Main;
